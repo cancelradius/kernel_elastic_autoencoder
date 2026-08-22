6e190ea3342ca64f350bbc91d2f4889619e25b77
felix <felix.rotter@mail.utoronto.ca>
2026-08-22 00:23:28 -0400

    feat(api)!: streamline API by changing sampler and collator interfaces
    Streamline public-facing API by removing the Sampler interface and hard-coding the default greedy sampler, and removing the Collator interface entirely. Thus, users can pass only a pretrained model and tokenizer and begin inference without the trouble of instantiating these objects.

--- src/kernel_elastic_autoencoder/__init__.py
+++ src/kernel_elastic_autoencoder/__init__.py
@@ -1,4 +1,4 @@
-from kernel_elastic_autoencoder.collate import Collated, Collator, DataframeCollator
+from kernel_elastic_autoencoder.collate import Collated
 from kernel_elastic_autoencoder.config import (
     ExperimentConfig,
     ModelCommonConfig,
@@ -14,15 +14,12 @@ from kernel_elastic_autoencoder.config import (
 from kernel_elastic_autoencoder.losses import Loss
 from kernel_elastic_autoencoder.model import Model
 from kernel_elastic_autoencoder.pipeline import Completion, Pipeline
-from kernel_elastic_autoencoder.sample import Sampler, Top1Sampler
 from kernel_elastic_autoencoder.tokenizer import Tokenizer
 from kernel_elastic_autoencoder.training import Trainer, TrainerCallback
 
 __all__ = [
     "Collated",
-    "Collator",
     "Completion",
-    "DataframeCollator",
     "ExperimentConfig",
     "Loss",
     "Model",
@@ -32,9 +29,7 @@ __all__ = [
     "ModelEncoderConfig",
     "ModelInputConfig",
     "Pipeline",
-    "Sampler",
     "Tokenizer",
-    "Top1Sampler",
     "Trainer",
     "TrainerCallback",
     "TrainingCommonConfig",
--- src/kernel_elastic_autoencoder/pipeline.py
+++ src/kernel_elastic_autoencoder/pipeline.py
@@ -1,17 +1,14 @@
-from collections.abc import Callable, Iterable
+from collections.abc import Iterable
+from types import SimpleNamespace
 
 import torch
 from pydantic import BaseModel, ConfigDict
 
-from kernel_elastic_autoencoder.collate import Collator, DataframeCollator
-from kernel_elastic_autoencoder.config import ModelConfig
 from kernel_elastic_autoencoder.model import Model
-from kernel_elastic_autoencoder.sample import Sampler, Top1Sampler
 from kernel_elastic_autoencoder.tokenizer import Tokenizer
 
 
-class CompletionIntermediate(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
+class CompletionIntermediate(SimpleNamespace):
     latents: torch.Tensor
     input_ids: torch.Tensor
     batches_completed: torch.Tensor
@@ -19,10 +16,9 @@ class CompletionIntermediate(BaseModel):
     condition_mask: torch.Tensor
 
 
-class Completion(BaseModel):
+class Completion(SimpleNamespace):
     """Return schema for Pipeline.completion."""
 
-    model_config = ConfigDict(arbitrary_types_allowed=True)
     outputs: Iterable[str]
     """Iterable of generated output sequences."""
     condition_embeddings: torch.Tensor
@@ -31,7 +27,7 @@ class Completion(BaseModel):
     """Tensor containing generated condition masks for reuse."""
 
 
-class Pipeline[T]:
+class Pipeline:
     """User-facing pipeline for inference.
 
     Defines an easy-to-use API for decoder-only inference with a pretrained model.
@@ -39,165 +35,149 @@ class Pipeline[T]:
     Examples:
         Wrapping a pretrained model and tokenizer:
         >>> model = Model.from_pretrained("./checkpoint")
-        >>> tokenizer = MyClassImplementingTokenizer.from_pretrained("./checkpoint/tokenizer")
-        >>> pipe = Pipeline(model, tokenizer, MyClassImplementingCollator, MyClassImplementingSampler)
-        >>> compl = pipe.completion(latents, dataset, seq_feature, cond_features)
-        >>> print(compl.outputs)
+        >>> tokenizer = MyTokenizer.from_pretrained("./checkpoint/tokenizer")
+        >>> pipe = Pipeline(model, tokenizer)
 
-        TODO: Complete examples.
+        Getting a completion for sequences:
+        >>> compl = pipe.completion(latents, ["abc", "def", "ghi"], [[1.0, 0.5], [2.0, 1.0], [3.0, 1.5]])
+        >>> print(compl.outputs)
     """
 
     def __init__(
         self,
         model: Model,
         tokenizer: Tokenizer,
-        collator: Callable[[ModelConfig, Tokenizer], Collator[T]] = DataframeCollator,
-        sampler: Callable[[Tokenizer], Sampler] = Top1Sampler,
         device: torch.device | None = None,
     ) -> None:
         """Instantiates a Pipeline object.
 
-        TODO: Pipeline setup will be streamlined such that collators and samplers are by default fetched from model
-            configuration.
-
         Args:
             model: Pre-trained Model object for inference. Can be obtained from Model.from_pretrained.
-            tokenizer: Pre-configured Tokenizer object. The Tokenizer protocol supports tokenizers inheriting from
-                transformers.PreTrainedTokenizerBase, so such tokenizers may be loaded from HuggingFace Hub.
-            collator: Constructor for a data collator inheriting from Collator. Determines input types to Pipeline methods.
-            sampler: Constructor for a data sampler inheriting from Sampler. Determines how outputs are produced from
-                Pipeline methods.
+            tokenizer: Pre-configured Tokenizer object. The Tokenizer protocol supports tokenizers
+                inheriting from transformers.PreTrainedTokenizerBase, so such tokenizers may be loaded
+                from HuggingFace Hub.
             device: Torch device used for inference.
         """
         self.model = model.to(device)
         """Pre-trained Model object for inference. Moved to Pipeline.device, and placed in eval() mode."""
         self.model.eval()
         self.tokenizer = tokenizer
-        """Pre-configured Tokenizer object. The Tokenizer Protocol supports tokenizers inheriting from
-            transformers.PreTrainedTokenizerBase, so such tokenizers may be loaded from HuggingFace Hub."""
+        """Pre-configured Tokenizer object."""
         self.device = device
         """Torch device used for inference."""
 
-        self.collator = collator(model.config_typed, tokenizer)
-        """Data collator inheriting from Collator, instantiated by passing Pipeline.model.config_typed and Pipeline.tokenizer."""
-        self.sampler = sampler(tokenizer)
-        """Data sampler inheriting from Sampler, instantiated by passing Pipeline.tokenizer."""
+    def _ingest(
+        self,
+        sequences: list[str],
+        conditions: list[list[float]] | torch.Tensor,
+        device: torch.device | None,
+        **kwargs,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        input_ids = self.tokenizer.encode(
+            seq=sequences,
+            padding=False,
+            max_length=self.model.config_typed.input.max_len,
+            add_special_tokens=False,
+            **kwargs,
+        )
+        conditions = torch.as_tensor(conditions, dtype=torch.float, device=device)
+        condition_mask = (
+            conditions != self.model.config_typed.common.padding_value
+        ).to(torch.bool)
+        return input_ids, conditions, condition_mask
 
     @torch.inference_mode()
     def _completion_entry(
         self,
         latents: torch.Tensor,
-        dataset: T,
-        seq_feature: str,
-        cond_features: list[str],
+        sequences: list[str],
+        conditions: list[list[float]] | torch.Tensor,
         device: torch.device | None = None,
         **kwargs,
-    ) -> CompletionIntermediate:
-        ds = self.collator(
-            dataset=dataset,
-            seq_feature=seq_feature,
-            cond_features=cond_features,
-            padding=False,
-            add_special_tokens=False,
-            device=device,
-            **kwargs,
+    ) -> dict[str, torch.Tensor]:
+        input_ids, conds, cond_mask = self._ingest(
+            sequences, conditions, device, **kwargs
         )
-        conds_embed = self.model.embed_conditions(ds.conditions)
-        return CompletionIntermediate(
-            latents=latents,
-            input_ids=ds.input_ids,
-            batches_completed=torch.zeros(ds.input_ids.size(0), dtype=torch.bool),
-            condition_embeddings=conds_embed,
-            condition_mask=ds.condition_mask,
-        )
+        conds_embed = self.model.embed_conditions(conds)
+        return {
+            "latents": latents,
+            "input_ids": input_ids,
+            "batches_completed": torch.zeros(input_ids.size(0), dtype=torch.bool),
+            "condition_embeddings": conds_embed,
+            "condition_mask": cond_mask,
+        }
 
     @torch.inference_mode()
     def _completion_step(
-        self, intermediate: CompletionIntermediate, **kwargs
-    ) -> CompletionIntermediate:
-        intermediate.input_ids = torch.cat(
+        self, intermediate: dict[str, torch.Tensor]
+    ) -> dict[str, torch.Tensor]:
+        intermediate["input_ids"] = torch.cat(
             [
                 torch.full(
-                    (intermediate.input_ids.size(0), 1), self.tokenizer.bos_token_id
+                    (intermediate["input_ids"].size(0), 1), self.tokenizer.bos_token_id
                 ),
-                intermediate.input_ids,
+                intermediate["input_ids"],
             ],
             dim=1,
         )
         logits = self.model.decode(
-            current_output=intermediate.input_ids,
-            latents=intermediate.latents,
-            condition_embeddings=intermediate.condition_embeddings,
+            current_output=intermediate["input_ids"],
+            latents=intermediate["latents"],
+            condition_embeddings=intermediate["condition_embeddings"],
             token_mask=None,
-            condition_mask=intermediate.condition_mask,
+            condition_mask=intermediate["condition_mask"],
         )
-        new_toks = self.sampler.sample_ids(logits[:, -1:])
-        intermediate.batches_completed |= (
+        new_toks = (
+            torch.topk(logits[:, -1:], k=1, dim=-1).indices.squeeze(-1).to(torch.long)
+        )
+        intermediate["batches_completed"] |= (
             new_toks.squeeze(-1) == self.tokenizer.eos_token_id
         )
-        torch.where(
-            intermediate.batches_completed.unsqueeze(-1),
+        new_toks = torch.where(
+            intermediate["batches_completed"].unsqueeze(-1),
             self.tokenizer.pad_token_id,
             new_toks,
         )
-        intermediate.input_ids = torch.cat([intermediate.input_ids, new_toks], dim=1)
+        intermediate["input_ids"] = torch.cat([intermediate["input_ids"], new_toks], dim=1)
         return intermediate
 
     @torch.inference_mode()
-    def _completion_exit(
-        self, intermediate: CompletionIntermediate, **kwargs
-    ) -> Completion:
-        return Completion(
-            outputs=self.sampler(
-                intermediate.input_ids, skip_special_tokens=True
-            ),
-            condition_embeddings=intermediate.condition_embeddings,
-            condition_mask=intermediate.condition_mask,
-        )
+    def _completion_exit(self, intermediate: dict[str, torch.Tensor]) -> Iterable[str]:
+        return self.tokenizer.decode(intermediate["input_ids"], skip_special_tokens=True)
 
     def completion(
         self,
         latents: torch.Tensor,
-        dataset: T,
-        seq_feature: str,
-        cond_features: list[str],
+        sequences: list[str],
+        conditions: list[list[float]] | torch.Tensor,
         device: torch.device | None = None,
         **kwargs,
-    ) -> Completion:
-        """Completes each batch in an input dataset with latent and condition guidance through the decoder.
+    ) -> Iterable[str]:
+        """Completes each conditioned input sequence.
 
-        Using decoder-only inference, the model completes each batch in the provided dataset. Dataset input typing is
-        uniquely defined by the used Collator. The input dataset is passed through the collator, then subject to
-        autoregressive decoder-only inference through the model, where new tokens are sampled from intermediates and
-        stored in a tensor format. Finally, on reaching end-of-sequence tokens or the maximum sequence length, the
-        sequence is decoded by the tokenizer and returned along with the generated condition embeddings and masks in a
-        Completion schema.
-
-        TODO: Callbacks are planned to enable easy custom inference functions and streaming of text.
+        The model completes each sequence in the provided list using decoder-only inference. Tokens are
+        sampled greedily.
 
         Args:
-            latents: Tensor of dimension (B, P * E) containing latent vectors for the batches.
-            dataset: Dataset of type specified in Pipeline.collator.
-            seq_feature: String to pass to collator containing the feature or column containing text sequences to
-                complete.
-            cond_features: List of strings to pass to collator containing features or columns containing numerical
-                condition values.
+            latents: Tensor of dimension (B, P * E) containing latent vectors for the batch.
+            sequences: List of text sequences to complete.
+            conditions: List of condition value lists per batch.
             device: Torch device used for inference.
-            **kwargs: Additional keyword arguments passed to Pipeline.collator.
+            **kwargs: Additional keyword arguments passed to Tokenizer.encode.
 
         Returns:
-            Completion: Completion object, containing outputs, as well as condition embeddings and masks for reuse.
+            Completion: Completion object, containing outputs, as well as condition embeddings and
+                masks for reuse.
         """
         intermediate = self._completion_entry(
             latents=latents,
-            dataset=dataset,
-            seq_feature=seq_feature,
-            cond_features=cond_features,
+            sequences=sequences,
+            conditions=conditions,
             device=device,
             **kwargs,
         )
         while (
-            intermediate.input_ids.size(1) < self.model.config_typed.input.max_len
-        ) and (not intermediate.batches_completed.all()):
+            intermediate["input_ids"].size(1) < self.model.config_typed.input.max_len
+        ) and (not intermediate["batches_completed"].all()):
             intermediate = self._completion_step(intermediate=intermediate)
         return self._completion_exit(intermediate=intermediate)
