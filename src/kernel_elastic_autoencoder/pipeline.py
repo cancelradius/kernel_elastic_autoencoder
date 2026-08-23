a5da58284830d6a5fb331e52b3a4b3b4f1417774
felix <felix.rotter@mail.utoronto.ca>
2026-08-23 15:22:39 -0400

    refactor: flatten Pipeline.completion and refactor away private methods
    Remove unnecessary private method splitting in Pipeline.completion and flatten into a single public method for easier maint.

--- src/kernel_elastic_autoencoder/pipeline.py
+++ src/kernel_elastic_autoencoder/pipeline.py
@@ -45,85 +45,6 @@ class Pipeline:
         self.device = device
         """Torch device used for inference."""
 
-    def _ingest(
-        self,
-        sequences: list[str],
-        conditions: list[list[float]] | torch.Tensor,
-        device: torch.device | None,
-        **kwargs,
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        input_ids = self.tokenizer.encode(
-            seq=sequences,
-            padding=False,
-            max_length=self.model.config_typed.input.max_len,
-            add_special_tokens=False,
-            **kwargs,
-        ).to(self.device)
-        conditions = torch.as_tensor(conditions, dtype=torch.float, device=device)
-        condition_mask = (
-            conditions != self.model.config_typed.common.padding_value
-        ).to(torch.bool)
-        return input_ids, conditions, condition_mask
-
-    @torch.inference_mode()
-    def _completion_entry(
-        self,
-        latents: torch.Tensor,
-        sequences: list[str],
-        conditions: list[list[float]] | torch.Tensor,
-        device: torch.device | None = None,
-        **kwargs,
-    ) -> dict[str, torch.Tensor]:
-        input_ids, conds, cond_mask = self._ingest(
-            sequences, conditions, device, **kwargs
-        )
-        conds_embed = self.model.embed_conditions(conds)
-        return {
-            "latents": latents,
-            "input_ids": input_ids,
-            "batches_completed": torch.zeros(input_ids.size(0), dtype=torch.bool),
-            "condition_embeddings": conds_embed,
-            "condition_mask": cond_mask,
-        }
-
-    @torch.inference_mode()
-    def _completion_step(
-        self, intermediate: dict[str, torch.Tensor]
-    ) -> dict[str, torch.Tensor]:
-        intermediate["input_ids"] = torch.cat(
-            [
-                torch.full(
-                    (intermediate["input_ids"].size(0), 1), self.tokenizer.bos_token_id
-                ),
-                intermediate["input_ids"],
-            ],
-            dim=1,
-        )
-        logits = self.model.decode(
-            current_output=intermediate["input_ids"],
-            latents=intermediate["latents"],
-            condition_embeddings=intermediate["condition_embeddings"],
-            token_mask=None,
-            condition_mask=intermediate["condition_mask"],
-        )
-        new_toks = (
-            torch.topk(logits[:, -1:], k=1, dim=-1).indices.squeeze(-1).to(torch.long)
-        )
-        intermediate["batches_completed"] |= (
-            new_toks.squeeze(-1) == self.tokenizer.eos_token_id
-        )
-        new_toks = torch.where(
-            intermediate["batches_completed"].unsqueeze(-1),
-            self.tokenizer.pad_token_id,
-            new_toks,
-        )
-        intermediate["input_ids"] = torch.cat([intermediate["input_ids"], new_toks], dim=1)
-        return intermediate
-
-    @torch.inference_mode()
-    def _completion_exit(self, intermediate: dict[str, torch.Tensor]) -> Iterable[str]:
-        return self.tokenizer.decode(intermediate["input_ids"], skip_special_tokens=True)
-
     def completion(
         self,
         latents: torch.Tensor,
@@ -147,15 +68,54 @@ class Pipeline:
         Returns:
             Iterable[str]: List of completed sequences, stripped of special tokens.
         """
-        intermediate = self._completion_entry(
-            latents=latents,
-            sequences=sequences,
-            conditions=conditions,
-            device=device,
+        input_ids = self.tokenizer.encode(
+            seq=sequences,
+            padding=False,
+            max_length=self.model.config_typed.input.max_len,
+            add_special_tokens=False,
             **kwargs,
-        )
+        ).to(self.device)
+        conditions = torch.as_tensor(conditions, dtype=torch.float, device=device)
+        condition_mask = (
+            conditions != self.model.config_typed.common.padding_value
+        ).to(torch.bool)
+        conds_embed = self.model.embed_conditions(conditions)
+        batches_completed = torch.zeros(input_ids.size(0), dtype=torch.bool)
+        
         while (
-            intermediate["input_ids"].size(1) < self.model.config_typed.input.max_len
-        ) and (not intermediate["batches_completed"].all()):
-            intermediate = self._completion_step(intermediate=intermediate)
-        return self._completion_exit(intermediate=intermediate)
+            input_ids.size(1) < self.model.config_typed.input.max_len
+        ) and (not batches_completed.all()):
+            input_ids = torch.cat(
+                [
+                    torch.full(
+                        (input_ids.size(0), 1),
+                        self.tokenizer.bos_token_id,
+                    ),
+                    input_ids,
+                ],
+                dim=1,
+            )
+            logits = self.model.decode(
+                current_output=input_ids,
+                latents=latents,
+                condition_embeddings=conds_embed,
+                token_mask=None,
+                condition_mask=condition_mask,
+            )
+            new_toks = (
+                torch.topk(logits[:, -1:], k=1, dim=-1)
+                .indices.squeeze(-1)
+                .to(torch.long)
+            )
+            batches_completed |= (
+                new_toks.squeeze(-1) == self.tokenizer.eos_token_id
+            )
+            new_toks = torch.where(
+                batches_completed.unsqueeze(-1),
+                self.tokenizer.pad_token_id,
+                new_toks,
+            )
+            input_ids = torch.cat(
+                [input_ids, new_toks], dim=1
+            )
+        return self.tokenizer.decode(input_ids, skip_special_tokens=True)
