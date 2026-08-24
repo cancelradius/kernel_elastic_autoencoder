c09abb4970cd68014e2765ab3e9a54fa327eabce
felix <felix.rotter@mail.utoronto.ca>
2026-08-24 16:38:53 -0400

    feat: add beam search algorithm to Pipeline API
    Add beam search decoding strategy described in the original paper to Pipeline.

--- src/kernel_elastic_autoencoder/pipeline.py
+++ src/kernel_elastic_autoencoder/pipeline.py
@@ -81,10 +81,10 @@ class Pipeline:
         ).to(torch.bool)
         conds_embed = self.model.embed_conditions(conditions)
         batches_completed = torch.zeros(input_ids.size(0), dtype=torch.bool)
-        
-        while (
-            input_ids.size(1) < self.model.config_typed.input.max_len
-        ) and (not batches_completed.all()):
+
+        while (input_ids.size(1) < self.model.config_typed.input.max_len) and (
+            not batches_completed.all()
+        ):
             input_ids = torch.cat(
                 [
                     torch.full(
@@ -107,15 +107,123 @@ class Pipeline:
                 .indices.squeeze(-1)
                 .to(torch.long)
             )
-            batches_completed |= (
-                new_toks.squeeze(-1) == self.tokenizer.eos_token_id
-            )
+            batches_completed |= new_toks.squeeze(-1) == self.tokenizer.eos_token_id
             new_toks = torch.where(
                 batches_completed.unsqueeze(-1),
                 self.tokenizer.pad_token_id,
                 new_toks,
             )
-            input_ids = torch.cat(
-                [input_ids, new_toks], dim=1
-            )
+            input_ids = torch.cat([input_ids, new_toks], dim=1)
         return self.tokenizer.decode(input_ids, skip_special_tokens=True)
+
+    def beam_completion(
+        self,
+        latents: torch.Tensor,
+        beam_size: int,
+        sequences: list[str],
+        conditions: list[list[float]] | torch.Tensor,
+        device: torch.device | None = None,
+        **kwargs,
+    ) -> Iterable[str]:
+        input_ids = self.tokenizer.encode(
+            seq=sequences,
+            padding=False,
+            max_length=self.model.config_typed.input.max_len,
+            add_special_tokens=False,
+            **kwargs,
+        ).to(self.device)
+        input_probs = torch.zeros_like(input_ids * beam_size)
+        conditions = torch.as_tensor(conditions, dtype=torch.float, device=device)
+        condition_mask = (
+            (conditions != self.model.config_typed.common.padding_value)
+            .to(torch.bool)
+            .repeat_interleave(beam_size, dim=0)
+        )
+        conds_embed = self.model.embed_conditions(conditions).repeat_interleave(
+            beam_size, dim=0
+        )
+        batches_completed = torch.zeros(input_ids.size(0) * beam_size, dtype=torch.bool)
+
+        logits = self.model.decode(
+            current_output=input_ids,
+            latents=latents,
+            condition_embeddings=conds_embed,
+            token_mask=None,
+            condition_mask=condition_mask,
+        )
+        new_toks = (
+            torch.topk(logits[:, -1:], k=beam_size, dim=-1)
+            .indices.squeeze(-1)
+            .to(torch.long)
+        )
+        new_probs = (
+            torch.topk(logits[:, -1:], k=beam_size, dim=-1)
+            .values.squeeze(-1)
+            .to(torch.long)
+        )
+        batches_completed |= new_toks.squeeze(-1) == self.tokenizer.eos_token_id
+        new_toks = torch.where(
+            batches_completed.unsqueeze(-1),
+            self.tokenizer.pad_token_id,
+            new_toks,
+        )
+        new_probs = torch.where(
+            batches_completed.unsqueeze(-1),
+            0.0,
+            new_probs,
+        )
+        input_ids = torch.cat(
+            [input_ids.repeat_interleave(beam_size, dim=0), new_toks], dim=1
+        )
+        input_probs = torch.cat([input_probs, new_probs], dim=1)
+
+        while (input_ids.size(1) < self.model.config_typed.input.max_len) and (
+            not batches_completed.all()
+        ):
+            input_ids = torch.cat(
+                [
+                    torch.full(
+                        (input_ids.size(0), 1),
+                        self.tokenizer.bos_token_id,
+                    ),
+                    input_ids,
+                ],
+                dim=1,
+            )
+            logits = self.model.decode(
+                current_output=input_ids,
+                latents=latents,
+                condition_embeddings=conds_embed,
+                token_mask=None,
+                condition_mask=condition_mask,
+            )
+            new_toks = (
+                torch.topk(logits[:, -1:], k=1, dim=-1)
+                .indices.squeeze(-1)
+                .to(torch.long)
+            )
+            new_probs = (
+                torch.topk(logits[:, -1:], k=1, dim=-1)
+                .values.squeeze(-1)
+                .to(torch.long)
+            )
+            batches_completed |= new_toks.squeeze(-1) == self.tokenizer.eos_token_id
+            new_toks = torch.where(
+                batches_completed.unsqueeze(-1),
+                self.tokenizer.pad_token_id,
+                new_toks,
+            )
+            new_probs = torch.where(
+                batches_completed.unsqueeze(-1),
+                0.0,
+                new_probs,
+            )
+            input_ids = torch.cat([input_ids, new_toks], dim=1)
+            input_probs = torch.cat([input_probs, new_probs], dim=1)
+
+        top_probs = input_probs.reshape(input_probs.size(0) // beam_size, beam_size, -1)
+        top_prob_inds = top_probs.sum(dim=-1).topk(k=1, dim=1).indices
+        winning_ids = torch.take_along_dim(
+            input_ids, top_prob_inds.unsqueeze(-1), 1
+        ).squeeze(1)
+        return self.tokenizer.decode(winning_ids, skip_special_tokens=True)
