fix: correctly sized causal mask buffer
Fix an error where causal mask is buffered at size (max_len-1, max_len-1)

src/kernel_elastic_autoencoder/layers.py
@@ -205,7 +205,7 @@ class Decoder(nn.Module):
         self.out_linear = nn.Linear(embedding_dim, vocab_size)
 
         self.register_buffer(
-            "causal_mask", nn.Transformer.generate_square_subsequent_mask(max_len - 1).to(torch.bool)
+            "causal_mask", nn.Transformer.generate_square_subsequent_mask(max_len).to(torch.bool)
         )
 
     def forward(